e5b4eda2c2dcc486650b1e59f161d9d6b19d57a3
Federico Degraf <fedegraf98@gmail.com>
2020-09-23 11:38:34 -0300

    Ataque de Player + Enemigo Mele
    Animacion + evento de ataque + LifeManager de Player y enemigo mele

--- TP Programación 1/Assets/Scripts/CharacterInput.cs
+++ TP Programación 1/Assets/Scripts/CharacterInput.cs
@@ -8,22 +8,17 @@ using UnityEngine.UIElements;
 
 public class CharacterInput : MonoBehaviour
 {
-    /*
-    [SerializeField] private string leftKey;
-    private bool leftCondition;
-    [SerializeField] private string rightKey;
-    private bool rightCondition;
-    [SerializeField] private string attackKey;
-    private bool attackCondition;
-    [SerializeField] private string jumpKey;
-    private bool jumpCondition;
-    */
 
     [SerializeField] private float speed = 1;
-
+    [SerializeField] private bool lookFront = true;
+    private bool isAlive;
+    private Animator animatorController;
+    [SerializeField] private GameObject attackZone;
+    [SerializeField] private float tiempoDeAtaque;
+    private float attackTimer;
     void Start()
     {
-/*
+        /*
 Se termino resolviendo de otra forma, pero la idea es utiliarlo
         leftKey = "a";
         rightKey = "d";
@@ -32,50 +27,91 @@ Se termino resolviendo de otra forma, pero la idea es utiliarlo
 */
         
     }
+    public void GetKilled()
+    {
+        isAlive = false;
+        attackTimer = 0;
+    }
     void Update()
     {
+    if (isAlive)
+    {
+            float xMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.position += new Vector3(xMovement, 0f, 0f);
+            bool keyRightDown = Input.GetKeyDown(KeyCode.A);
+            bool keyLeftDown = Input.GetKeyDown(KeyCode.D);
+            bool TriggerAttack = Input.GetKeyDown(KeyCode.C);
+            bool KeyRightUp = Input.GetKeyUp(KeyCode.A);
+            bool keyLeftUp = Input.GetKeyUp(KeyCode.D);
+            if (keyRightDown && lookFront)
+            {
+               transform.Rotate(0,180,0);
+               lookFront = false;
+            }
+    
+            if (keyLeftDown && !lookFront)
+            {
+               transform.Rotate(0,180,0);
+               lookFront = true;
+            }
+    
+            if (keyLeftUp || KeyRightUp && !(keyLeftDown || keyRightDown))
+            {
+                animatorController.SetBool("IsRunning", false);
+               // Debug.Log("Dejo de correr");
+            }
+    
+            if (keyLeftDown || keyRightDown)
+            {
+                animatorController.SetBool("IsRunning", true);
+            }
+            if (TriggerAttack)
+            {
+                Attack();
+            }
 
-        float xMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        transform.position += new Vector3(xMovement, 0f, 0f);
-        /*
-        leftCondition = Input.GetKey(KeyCode.A);
-        rightCondition = Input.GetKey(KeyCode.D);
-        jumpCondition = Input.GetKey(KeyCode.Space);
-        attackCondition = Input.GetKey(KeyCode.K);
-        bool keyRightDown = Input.GetKeyDown(KeyCode.D);
-        bool keyLeftDown = Input.GetKeyDown(KeyCode.A);
-        if (leftCondition)
-        {
-            /*float XAxis = 1 * -speed * Time.deltaTime;
-            transform.Translate(new Vector3(XAxis, 0, 0)); 
-        }
-        if (attackCondition)
-        {
-            Debug.Log("Evento ataque");            
-        }
-        if (jumpCondition)
-        {
-            Debug.Log("Evento Salto");
-        }
-        if (rightCondition)
-        {
-            float XAxis = 1 * speed * Time.deltaTime;
-            transform.Translate(new Vector3(XAxis, 0, 0));
-        }
-        if (keyRightDown && this.transform.rotation.y != 180)
-        {
-            transform.Rotate(0,180,0);
-        }
+            if (attackZone.activeInHierarchy)
+            {
+                if (attackTimer >= tiempoDeAtaque)
+                {
+                    attackZone.SetActive(false);
+                    attackTimer = 0;
+                }
+                else
+                {
+                    attackTimer += Time.deltaTime;
+                }
+            }
+    }
 
-        if (keyLeftDown && this.transform.rotation.y != 0)
-        {
-            transform.Rotate(0,180,0);
-        }
-        */
+    /*
+    leftCondition = Input.GetKey(KeyCode.A);
+    rightCondition = Input.GetKey(KeyCode.D);
+    jumpCondition = Input.GetKey(KeyCode.Space);
+    attackCondition = Input.GetKey(KeyCode.K);
+    if (leftCondition)
+    {
+        /*float XAxis = 1 * -speed * Time.deltaTime;
+        transform.Translate(new Vector3(XAxis, 0, 0)); 
+    }
+    if (jumpCondition)
+    {
+        Debug.Log("Evento Salto");
+    }
+    if (rightCondition)
+    {
+        float XAxis = 1 * speed * Time.deltaTime;
+        transform.Translate(new Vector3(XAxis, 0, 0));
+    }
+    */
     }
 
     void Awake()
     {
+        animatorController = GetComponent<Animator>();
+        isAlive = true;
+        attackZone.SetActive(false);
+        attackTimer = 0;
     }
     private void MoveLeft()
     {}
@@ -85,6 +121,8 @@ Se termino resolviendo de otra forma, pero la idea es utiliarlo
     {}
     private void Attack()
     {
-        
+        Debug.Log("Evento ataque");
+        animatorController.SetTrigger("attackEvent");
+        attackZone.SetActive(true);
     }
 }
---
+++ TP Programación 1/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private float maxLife;
+    private float currentLife;
+    private Animator animatorController;
+
+    private CharacterInput characterinputScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLife = maxLife;
+    }
+
+    private void Awake()
+    {
+        animatorController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamage(float damage)
+    {
+        if (currentLife > 0)
+        {
+          //  Debug.Log("Fue invocado Get Damage");
+            currentLife -= damage;
+          //  Debug.Log("currentLife: " + currentLife);
+            if (currentLife <= 0)
+            {
+                animatorController.SetBool("Dead", true);
+                characterinputScript.GetKilled();
+            }
+            else
+            {
+                animatorController.SetTrigger("GetHitted");
+            }   
+        }
+    }
+}
